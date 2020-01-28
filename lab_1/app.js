f238f8a295ba229873b42d26bf93b87490022d0d
ismail adam <www.vvcs-fat@hotmail.ac.uk>
2020-01-28 15:53:52 +0000

    make it better

--- lab_1/app.js
+++ lab_1/app.js
@@ -1,26 +1,41 @@
-var http = require('http');
+var http = require('http'),
+        url = require("url"),
+    fs = require('fs'),
+    path = require("path");
+var staticBasePath = './static';
+
+function serveStaticFile(res, path, contentType, responseCode) {
+    if(!responseCode) responseCode = 200;
+    fs.readFile(__dirname + path, function(err,data) {
+        if(err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('500 - Internal Error');
+        } else {
+            res.writeHead(responseCode,{ 'Content-Type': contentType });
+            res.end(data);
+        }
+    });
+}
 
 http.createServer(function(req,res){
-    // the replace function removes any query strings and slashes
-    // the toLowerCase functions makes it lower case 
+    // normalize url by removing querystring, optional
+    // trailing slash, and making lowercasevar 
     path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
+    
     switch(path) {
         case '':
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end('Homepage');
+            serveStaticFile(res, '/views/home.html', 'text/html');
             break;
         case '/about':
-            res.writeHead(200, { 'Content-Type': 'text/htm' });
-            res.end('Hello world');
+            serveStaticFile(res, '/views/about.html', 'text/html');
             break;
         case '/img/logo.jpg':
             serveStaticFile(res, '/public/img/logo.jpg', 'image/jpeg');
             break;
         default:
-            res.writeHead(404, { 'Content-Type': 'text/html' });
-            res.end('Not Found');
+            serveStaticFile(res, '/views/404.html', 'text/html',404);
             break;
-        }
-    }).listen(3000);
-    
+    }
+}).listen(3000);
+
 console.log('Server started on localhost:3000; press Ctrl-C to terminate....');
