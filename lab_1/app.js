271121395310ab4058616b77bcd00bf4e5b4859a
ismail adam <www.vvcs-fat@hotmail.ac.uk>
2020-01-28 15:13:18 +0000

    initial commit

---
+++ lab_1/app.js
@@ -0,0 +1,27 @@
+var http = require('http');
+
+http.set("view engine", "ejs");
+http.createServer(function(req,res){
+    // the replace function removes any query strings and slashes
+    // the toLowerCase functions makes it lower case 
+    path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
+    switch(path) {
+        case '':
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end('Homepage');
+            break;
+        case '/about':
+            res.writeHead(200, { 'Content-Type': 'text/htm' });
+            res.end('Hello world');
+            break;
+        case '/img/logo.jpg':
+            serveStaticFile(res, '/public/img/logo.jpg', 'image/jpeg');
+            break;
+        default:
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.end('Not Found');
+            break;
+        }
+    }).listen(3000);
+    
+console.log('Server started on localhost:3000; press Ctrl-C to terminate....');
